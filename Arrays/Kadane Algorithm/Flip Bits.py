b0353aaddd092359663db5898d2be95c5c7ef75b
nateshv54 <95923052+nateshv54@users.noreply.github.com>
2024-02-03 10:37:26 +0530

    Update Flip Bits.py

--- Arrays/Kadane Algorithm/Flip Bits.py
+++ Arrays/Kadane Algorithm/Flip Bits.py
@@ -1,3 +1,55 @@
+'''
+You are given an array of integers ARR[] of size N consisting of zeros and ones. You have to select a subset and flip bits of that subset. You have to return the count of maximum one’s that you can obtain by flipping chosen sub-array at most once.
+
+A flip operation is one in which you turn 1 into 0 and 0 into 1.
+
+For example:
+If you are given an array {1, 1, 0, 0, 1} then you will have to return the count of maximum one’s you can obtain by flipping anyone chosen sub-array at most once, so here you will clearly choose sub-array from the index 2 to 3 and then flip it's bits. So, the final array comes out to be {1, 1, 1, 1, 1} which contains five ones and so you will return 5.
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+1 <= T = 100
+1 <= N <= 10^4
+0 <= ARR[i] <= 1
+
+Time Limit: 1 sec
+Sample Input 1 :
+3
+5
+1 0 0 1 0
+4
+1 1 1 0
+5
+0 0 1 0 0
+Sample Output 1 :
+4
+4
+4
+Explanation For Sample Input 1:
+For the first test case:
+We can perform a flip operation in the range [1,2]. After the flip operation, the array is: 1 1 1 1 0
+and so the answer will be 4
+
+Similarly, in the second test case :
+We can perform a flip operation in the range [3,3]. After the flip operation, the array is: 1 1 1 1 
+and so the answer will be 4.
+
+Finally for the third test case :
+We can perform a flip operation in the range [0,4] 
+After the flip operation, the array is: 1 1 0 1 1 
+and so the answer will be 4
+Sample Input 2 :
+3
+5
+0 0 0 0 0
+5
+1 1 1 1 1
+8
+1 0 1 0 1 0 1 0
+Sample Output 2 :
+5
+5
+5
+'''
 from os import *
 from sys import *
 from collections import *
