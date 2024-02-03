dcc16057c6aa11442d546b1068e19dd63bad86ad
nateshv54 <95923052+nateshv54@users.noreply.github.com>
2024-02-03 10:39:43 +0530

    Update Maximum Sum Rectangle.py

--- Arrays/Kadane Algorithm/Maximum Sum Rectangle.py
+++ Arrays/Kadane Algorithm/Maximum Sum Rectangle.py
@@ -1,3 +1,45 @@
+'''
+You are given a matrix ‘ARR’ with ‘N’ rows and ‘M’ columns. Your task is to find the maximum sum rectangle in the matrix.
+
+Maximum sum rectangle is a rectangle with the maximum value for the sum of integers present within its boundary, considering all the rectangles that can be formed from the elements of that matrix.
+
+For Example
+Consider following matrix:
+
+The rectangle (1,1) to (3,3) is the rectangle with the maximum sum, i.e. 29.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints
+ 1 <= T <= 10
+ 1 <= M, N <= 75
+ -10^5 <= ARR[i][j] <= 10^5
+
+Time Limit: 1 sec
+Sample Input 1
+2
+1 2
+-1 1
+2 2
+-1 1
+2 2
+Sample Output 1
+1
+4
+Explanation of Input 1
+The maximum sum rectangle corresponding to the first test case is-
+
+The maximum sum rectangle corresponding to the second test case is-
+
+Sample Input 2
+1
+4 5
+1 2 -1 -4 -20
+-8 -3  4 2 1
+3  8 10 1 3
+-4 -1 1 7 -6
+Sample Output 2
+29'''
+
 from os import *
 from sys import *
 from collections import *
