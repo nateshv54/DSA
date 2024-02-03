bcbf5139ac1781554037a349bec01e33b9b1b8bd
nateshv54 <95923052+nateshv54@users.noreply.github.com>
2024-02-03 10:21:27 +0530

    Update Kth Smallest Natural Number After Removing Some Numbers.py

--- Arrays/Kth Smallest Natural Number After Removing Some Numbers.py
+++ Arrays/Kth Smallest Natural Number After Removing Some Numbers.py
@@ -1,3 +1,40 @@
+'''
+You have a list of all-natural numbers and an ‘arr’. Your task is to find the Kth smallest natural number after removing all numbers in ‘arr’ from the list of all-natural numbers.
+
+Note: ‘arr’ will be sorted in ascending order.
+
+For example:
+You are given, ‘arr’ = [1, 2, 4], ‘K’ = ‘5’, You have the natural numbers as [1, 2, 3, 4, 5, 6, 7 .. ], after removing all the integers in the array from natural numbers, we have, [3, 5, 6, 7, 8]. Here 8 is the 5th smallest number. Hence the answer is 8.
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+1 <= T <= 10
+1 <= N <= 10^3
+1 <= K <= 10^6
+1 <= arr[i] <= 10^5
+
+Time Limit: 1 sec.
+Sample Input 1:
+2
+3 5
+1 2 4
+4 5
+1 2 4 5
+Sample Output 1:
+8
+9
+Explanation:
+For the first test case,‘arr’ = [1, 2, 4], ‘K’ = ‘5’, You have the natural numbers as [1, 2, 3, 4, 5, 6, 7 .. ], after removing all the integers in the array from natural numbers, we have, [3, 5, 6, 7, 8]. Here 8 is the 5th smallest number. Hence the answer is 8.
+
+For the second test case,‘arr’ = [1, 2, 4, 5], ‘K’ = ‘5’, You have the natural numbers as [1, 2, 3, 4, 5, 6, 7 .. ], after removing all the integers in the array from natural numbers, we have, [3, 6, 7, 8, 9]. Here 9 is the 5th smallest number. Hence the answer is 9.
+Sample Input 2:
+2
+1 5
+5
+3 3
+2 3 4
+Sample Output 2:
+6
+6'''
 from typing import List
 
 def kthSmallest(arr: List[int], k: int) -> int:
